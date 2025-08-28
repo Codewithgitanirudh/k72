3f5481c1f61184500cf774e015fef4c331ec64db
anirudh <anirudhjangid0@gmail.com>
2025-08-28 14:03:46 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import './App.css'
+import { Route, Routes } from 'react-router-dom'
+import Home from './pages/Home'
+import Projects from './pages/Projects'
+import Agence from './pages/Agence'
+
+function App() {
+
+  return (
+    <div >
+     <Routes>
+      <Route path='/' element={<Home />}/>
+      <Route path='/projects' element={<Projects />}/>
+      <Route path='/agence' element={<Agence />}/>
+     </Routes>
+    </div>
+  )
+}
+
+export default App
