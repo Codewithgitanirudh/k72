ac6b4e7d5449f130ec96c95e2d2087a040b5fb3e
anirudh <anirudhjangid0@gmail.com>
2025-08-29 12:01:37 +0530

    Update styles in index.css and Home component; remove GSAP markers in Agence component

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -5,7 +5,7 @@ import HomeTopBanner from '../components/home/HomeTopBanner'
 
 const Home = () => {
   return (
-    <div className='relative mb-5 overflow-auto'>
+    <div className='relative mb-5 overflow-y-auto text-white'>
     <div className='h-screen w-screen fixed top-0 left-0'>
         <Video />
     </div>
